e7515e4e5684921ab1401f03d5e94d0a173e6803
satyasai10 <satyasaikumar0698@gmail.com>
2022-01-04 16:42:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to react</h1>
+      <h2>thank you for visit</h2>
+    </div>
+  );
+}
+
+export default App;
